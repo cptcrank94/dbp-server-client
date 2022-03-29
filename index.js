217749a011a169e1cd71dc1305d41aadb128471a
cptcrank94 <93883691+cptcrank94@users.noreply.github.com>
2022-03-29 03:25:12 +0200

    Fixed standard routing

--- index.js
+++ index.js
@@ -29,13 +29,13 @@ app.use(bodyParser.json());
 // Parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Starting route
+app.use('/', express.static(path.join(__dirname, "/client/build")));
+
 // Routes
 require("./app/routes/user.routes")(app);
 require("./app/routes/category.routes")(app);
 require("./app/routes/item.routes")(app);
-
-// Starting route
-app.use('/', express.static(path.join(__dirname, "/client/build")));
 
 const { API_PORT } = process.env;
 const PORT = process.env.PORT || API_PORT; 
