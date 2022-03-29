7c355eb54327a1731c9597c2f82d4a9fca97f330
cptcrank94 <93883691+cptcrank94@users.noreply.github.com>
2022-03-29 12:14:56 +0200

    fixed axios baseURL

--- app/client/src/http-common.js
+++ app/client/src/http-common.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
-var base_url = window.location.origin + ':';
 export default axios.create({
-    baseURL: base_url + process.env.PORT,
+    baseURL: '',
     headers: {
         "Content-Type": "application/json",
         'Access-Control-Allow-Origin': "*"
