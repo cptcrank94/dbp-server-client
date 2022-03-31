5dfaabcf6991f196f3a07a31dfb05003a457d55d
cptcrank94 <93883691+cptcrank94@users.noreply.github.com>
2022-03-31 11:32:45 +0200

    fixed baseURL

--- app/client/src/http-common.js
+++ app/client/src/http-common.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 export default axios.create({
-    baseURL: '',
+    baseURL: '/api',
     headers: {
         "Content-Type": "application/json",
         'Access-Control-Allow-Origin': "*"
